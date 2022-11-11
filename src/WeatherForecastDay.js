52d862d3ee79934814edc7a69a7cb84fd25e1050
saraikong <94483250+saraikong@users.noreply.github.com>
2022-11-11 00:18:35 -0600

    Update WeatherForecastDay.js

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,1 +1,12 @@
 import React from "react";
+
+export default function WeatherForecastDay() {
+  return (
+    <li>
+      <div className="card-body pt-2 pb-0 mb-n2">
+        <p className=" mb-n5">{props.day}</p>
+        <WeatherIcon icon={forecast[0].weather[0].icon} />
+      </div>
+    </li>
+  );
+}
